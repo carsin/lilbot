4ec04a899e7712e6fd9f44ed9b7a9b822fbaa44f
carsin <carsin@users.noreply.github.com>
2017-10-12 10:16:59 -1000

    initial genius api version

--- bot.js
+++ bot.js
@@ -1,19 +1,49 @@
 console.log("Bot is starting...");
-var Twit = require("twit");
-var config = require("./config.js");
 
-var T = new Twit(config);
+const config = require("./config.js");
+const Twit = require("twit");
+const geniusapi = require("genius-api");
 
-var tweet = {
-    status: "#test #twit"
+const Genius = new geniusapi(config.geniuscfg.clientAccessToken);
+
+// genius API does not have an artist entrypoint.
+// Instead, search for the artist => get a song by that artist => get API info on that song => get artist id
+
+geniusapi.prototype.getArtistIdByName = function getArtistIdByName(artistName) {
+    const normalizeName = name => name.replace(/\./g, '').toLowerCase();   // regex removes dots
+    const artistNameNormalized = normalizeName(artistName);
+
+    return this.search(artistName).then((response) => {
+        for (let i = 0; i < response.hits.length; i += 1) {
+            const hit = response.hits[i];
+            if (hit.type === 'song' && normalizeName(hit.result.primary_artist.name) === artistNameNormalized) {
+                return hit.result;
+            }
+        }
+    }).then(songInfo => songInfo.primary_artist.id);
 }
 
-T.post('statuses/update', tweet, tweeted)
+var artistId = Genius.getArtistIdByName('Lil Yachty').catch(err => console.error(err));
 
-function tweeted(err, data, response) {
-    if (err) {
-        console.log("Error: " + err);
-    } else {
-        console.log("Success: " + data);
-    }
-}
+Genius.songsByArtist(artistId, {
+    per_page: 50,
+    sort: 'popularity',
+}).then(songs => songs.map(song => song.url)); // has more song info like 'id', 'title', ...
+
+console.log("ID" + artistId);
+
+// var T = new Twit(config.twitcfg);
+
+// var tweet = {
+//     status: "#test #twit"
+// }
+
+// T.post('statuses/update', tweet, tweeted)
+
+// function tweeted(err, data, response) {
+//     if (err) {
+//         console.log("Error: " + err);
+//     } else {
+//         console.log("Success: " + data);
+//     }
+// }
