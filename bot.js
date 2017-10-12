5720622baada25703ed130b1061d62ccceb630ad
carsin <carsin@users.noreply.github.com>
2017-10-12 09:49:23 -1000

    add tweet functionality

--- bot.js
+++ bot.js
@@ -3,3 +3,17 @@ var Twit = require("twit");
 var config = require("./config.js");
 
 var T = new Twit(config);
+
+var tweet = {
+    status: "#test #twit"
+}
+
+T.post('statuses/update', tweet, tweeted)
+
+function tweeted(err, data, response) {
+    if (err) {
+        console.log("Error: " + err);
+    } else {
+        console.log("Success: " + data);
+    }
+}
