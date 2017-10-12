691555f950afef48ffb7b4ca3fdb5db38fcc914e
carsin <carsin@users.noreply.github.com>
2017-10-12 09:22:04 -1000

    fix tokens import

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 console.log("Bot is starting...");
 var Twit = require("twit");
-var tokens = require("tokens.js");
+var tokens = require("./tokens.js");
 
 var T = new Twit({
     consumer_key:         tokens.consumer_key,
     consumer_secret:      tokens.consumer_secret,
-    access_token:         tokens.acces_token,
+    access_token:         tokens.access_token,
     access_token_secret:  tokens.access_token_secret,
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+});
